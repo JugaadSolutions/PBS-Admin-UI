e1005753a9d80c5ae272c36f8314f00e11500488
shivakumar <shiv.mytrintrin@gmail.com>
2017-01-04 14:12:39 +0530

    ssh-keygen -f "/home/gwr-5/.ssh/known_hosts" -R 13.13.13.2

--- app/controllers.js
+++ app/controllers.js
@@ -1102,9 +1102,9 @@
                 $scope.employeesData = response.data;
                 $scope.employeesData.forEach(function (employee) {
                     employee.status = StatusService.getEmployeeStatus(employee.status);
-                    for (var i = 0; i < employee.smartCardDetails.length; i++) {
+                    /*for (var i = 0; i < employee.smartCardDetails.length; i++) {
                         employee.smartCardDetails[i].cardLevel = StatusService.getCardLevel(employee.smartCardDetails[i].smartCardId.cardLevel);
-                    }
+                    }*/
                     if (!employee.picture || employee.picture == '') {
                         employee.profilePicUrl = 'assets/images/no-avatar.png'
                     } else {
@@ -1461,16 +1461,16 @@
         };
 
         $scope.updateEmployee = function () {
-            $scope.employee.phoneNumber = $scope.employee.countryCode + '-' + $scope.employee.phoneNumber;
+            $scope.employee.phoneNumber =$scope.employee.phoneNumber;
             if ($scope.employee.emergencyContact.contactNumber) {
-                $scope.employee.emergencyContact.contactNumber = $scope.employee.emergencyContact.countryCode + '-' + $scope.employee.emergencyContact.contactNumber;
+                $scope.employee.emergencyContact.contactNumber =  $scope.employee.emergencyContact.contactNumber;
             } else {
                 $scope.employee.emergencyContact.contactNumber = "";
             }
             DataService.updateEmployee($scope.employee).then(function (response) {
                 if (!response.error) {
                     growl.success(response.message);
-                    window.location.reload();
+                   // window.location.reload();
                 } else {
                     growl.error(response.message);
                 }
@@ -1745,7 +1745,7 @@
                     if (!response.error) {
                         $scope.farePlans = response.data;
                         $scope.farePlans.forEach(function (farePlan) {
-                            $scope.selectPlan = farePlan.planName;
+                            $scope.membership.selectPlan = farePlan.planName;
                             if (farePlan.planName === farePlanName) {
                                 $scope.selectPlan = farePlan._id;
                             }
@@ -1771,7 +1771,7 @@
             DataService.updateMembership($scope.membership).then(function (response) {
                 if (!response.error) {
                     growl.success(response.message);
-                    window.location.reload();
+                    //window.location.reload();
                 } else {
                     growl.error(response.message);
                 }
@@ -1803,7 +1803,7 @@
             if (!response.error) {
                 $scope.dockingStations = response.data;
                 $scope.dockingStations.forEach(function (dockingStation) {
-                    dockingStation.status = StatusService.getDockingStationStatus(dockingStation.status);
+                    dockingStation.status = StatusService.getDockingStationStatus(dockingStation.operationStatus);
                 });
                 $scope.dockingStationsTable.reload();
             } else {
@@ -1906,7 +1906,8 @@
             minimumCyclesAlert: 0,
             name: '',
             dockingUnitIds: [],
-            ipAddress: ''
+            ipAddress: '',
+            subnet:0
         };
 
         $scope.cancelAddDockingStation = function () {
@@ -2358,9 +2359,9 @@
             if (!response.error) {
                 $scope.dockingPorts = response.data;
                 $scope.dockingPorts.forEach(function (dockingPort) {
-                    dockingPort.status = StatusService.getDockingPortStatus(dockingPort.status);
-                    dockingPort.unitNumber = dockingPort.dockingUnitId.unitNumber;
-                    dockingPort.name = dockingPort.dockingStationId.name;
+                    dockingPort.status = StatusService.getDockingPortStatus(dockingPort.portStatus);
+                    //dockingPort.unitNumber = dockingPort.dockingUnitId.unitNumber;
+                   // dockingPort.name = dockingPort.dockingStationId.name;
                 });
                 $scope.dockingPortsTable.reload();
             } else {
@@ -2682,7 +2683,7 @@
             if (!response.error) {
                 $scope.bicyclesData = response.data;
                 $scope.bicyclesData.forEach(function (bicycle) {
-                    bicycle.status = StatusService.getBicycleStatus(bicycle.status);
+                    bicycle.status = StatusService.getBicycleStatus(bicycle.vehicleStatus);
                     bicycle.location = StatusService.getBicycleLocation(bicycle.location);
                     if (!bicycle.picture || bicycle.picture == '') {
                         bicycle.profilePicUrl = 'assets/images/bicycle.jpg'
@@ -4100,7 +4101,7 @@
                 var portInfo = {
                     Name:data.portIds[i].dockingPortId.Name,
                     _id:data.portIds[i].dockingPortId._id
-                }
+                };
           var a= $scope.portSelections.push(portInfo);
 
                 $scope.selectedPort=function (a) {
@@ -4238,7 +4239,7 @@
                 var portInfo = {
                     PortID:data.portIds[i].dockingPortId.PortID
                   /*  _id:data.portIds[i].dockingPortId._id*/
-                }
+                };
                 $scope.portSelections.push(portInfo);
             }
         };
